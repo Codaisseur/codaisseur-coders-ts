0910a55464ccf68f7b85fc06122df087840c4822
Domenico Gemoli <domenicogemoli@gmail.com>
2020-12-29 10:28:44 +0100

    4. Fetching data with a thunk

--- src/store/feed/actions.ts
+++ src/store/feed/actions.ts
@@ -1,4 +1,9 @@
+import axios from "axios";
+import { Dispatch } from "redux";
+import { ReduxState } from "..";
 import { FeedAction, Post } from "./types";
+
+const API_URL = `https://codaisseur-coders-network.herokuapp.com`;
 
 export const startLoading = (): FeedAction => ({
   type: "feed/startLoading",
@@ -8,3 +13,18 @@ export const postsFetched = (morePosts: Post[]): FeedAction => ({
   type: "feed/postsFetched",
   payload: morePosts,
 });
+
+export const fetchNext5Posts = async (
+  dispatch: Dispatch,
+  getState: () => ReduxState
+) => {
+  dispatch(startLoading());
+
+  const offset = getState().feed.posts.length;
+
+  const res = await axios.get(`${API_URL}/posts?offset=${offset}&limit=5`);
+
+  const morePosts = res.data.rows;
+
+  dispatch(postsFetched(morePosts));
+};
